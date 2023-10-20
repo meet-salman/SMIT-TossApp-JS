4bc2c979bda5285b8baa291fc8c89efb4d508dae
Salman Ahmed <say2salmanahmed@gmail.com>
2023-10-20 19:22:14 +0500

    Toss App

--- app.js
+++ app.js
@@ -7,38 +7,45 @@ const result = document.querySelector('#result');
 const message = document.querySelector('#message');
 
 
-const num = Math.round(Math.random());
 
+head.addEventListener('click', function () {
+    let num = Math.round(Math.random());
 
-head.addEventListener('click' , function () {
     if (num === 1) {
         coin.innerHTML = '<img id="coin-image" src="./assets/head.png" alt="coin-image">'
         choice.innerHTML = 'You chose "Head".'
+        result.style.color = 'blue'
         result.innerHTML = 'You Win The Toss!'
         message.innerHTML = 'Refresh the page before the next toss.'
-    } 
-    else{
+    }
+    else {
         coin.innerHTML = '<img id="coin-image" src="./assets/tail.png" alt="coin-image">'
         choice.innerHTML = 'You chose "Head".'
+        result.style.color = 'red'
         result.innerHTML = 'You Loss The Toss!'
         message.innerHTML = 'Refresh the page before the next toss.'
     }
-})
+});
 
-tail.addEventListener('click' , function () {
+
+tail.addEventListener('click', function () {
+    let num = Math.round(Math.random());
+
     if (num === 0) {
         coin.innerHTML = '<img id="coin-image" src="./assets/tail.png" alt="coin-image">'
         choice.innerHTML = 'You chose "Tail".'
+        result.style.color = 'blue'
         result.innerHTML = 'You Win The Toss!'
         message.innerHTML = 'Refresh the page before the next toss.'
-    } 
-    else{
+    }
+    else {
         coin.innerHTML = '<img id="coin-image" src="./assets/head.png" alt="coin-image">'
         choice.innerHTML = 'You chose "Tail".'
+        result.style.color = 'red'
         result.innerHTML = 'You Loss The Toss!'
         message.innerHTML = 'Refresh the page before the next toss.'
     }
-})
+});
 
 
 
